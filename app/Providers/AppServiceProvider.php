d91bfffb4389b56d15def985698051957e42ba54
afandjo <tekorolandafandjo94@gmail.com>
2025-07-10 15:39:05 +0200

    Corrige boucle de redirection HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,31 +4,17 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-     public function boot()
-{
-    Schema::defaultStringLength(191);
-
-    if (env('APP_ENV') === 'production') {
-        
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+         
     }
-}
 }
